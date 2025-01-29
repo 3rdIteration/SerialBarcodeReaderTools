90d135e6fac798e1d53b99f6eb6807f673589a50
3rd Iteration <steve@cryptoguide.tips>
2025-01-29 10:55:59 -0500

    update

--- README.md
+++ README.md
@@ -1,12 +1,42 @@
-# GM65
+# GM65 Barcode Scanner
 
-## Setup
+# Setup
+## Requirements
+This module uses pyserial, so that needs to be installed via pip
+
+## Enabling USB Virtual Serial Port
+You can either connect the barcode scanner via a serial port or use the built-in virtual serial port function.
+
+To use this function, you need to scan the following QR code. (You may need to manually trigger scanning the first time to do this, but once enabled, this setting is persistant until reset)
+![](utility_qr_codes/enable_virtual_usb.png)
+
+## Factory Reset the Scanner
+To reset all of the settings back to their defaults, scan this code.
+
+![](utility_qr_codes/factory_reset.png)
+
+# Usage
+Connect GM65 with your computer using USB (Or a USB-Serial Interface), and make sure you are using the right port name in the gm65.py file.
+
+usage: gm65.py [-h] [--hw-version] [--sw-version] [--sw-year] [--capture] port
+
+Argument Parser
+
+positional arguments:
+  port          Serial port to use
+
+options:
+  -h, --help    show this help message and exit
+  --hw-version  Report hardware version and exit
+  --sw-version  Report software version and exit
+  --sw-year     Report software year and exit
+  --capture     Read a QR Code and Exit (Also default behavior if no other command given)
+
+# User Manual
 
 http://www.microtechnica.tv/support/manual/brm65_man.pdf
 
-Follow the manual and setup the folowing configurations.
 
-1. Command Triggered Mode
-2. USB virtual serial port
 
-Connect GM65 with your computer using USB, and make sure you are using the right port name in the gm65.py file.
+
+
--- gm65.py
+++ gm65.py
@@ -1,33 +1,58 @@
 import serial
 import time
+import argparse
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0)
-scan_duration = 5
+parser=argparse.ArgumentParser(description="Argument Parser")
+parser.add_argument("port", help="Serial port to use")
+parser.add_argument("--hw-version", action='store_true', help="Report hardware version and exit")
+parser.add_argument("--sw-version", action='store_true', help="Report software version and exit")
+parser.add_argument("--sw-year", action='store_true', help="Report software year and exit")
+parser.add_argument("--capture", action='store_true', help="Read a QR Code and Exit (Also default behavior if no other command given)")
+args=parser.parse_args()
 
-def start_scan():
+ser = serial.Serial(args.port, 9600, timeout=0)
+
+# Sets how long to wait for data from the scanner (Can be really short when just querying for settings)
+scan_duration = 0.5
+
+if args.hw_version:
+    # Command to read hardware version
+    scan_command = b'\x7E\x00\x07\x01\x00\xE1\x01\xAB\xCD'
+elif args.sw_version:
+    # Command to read software version
+    scan_command = b'\x7E\x00\x07\x01\x00\xE2\x01\xAB\xCD'
+elif args.sw_year:
+    # Command to read Software Year
+    scan_command = b'\x7E\x00\x07\x01\x00\xE3\x01\xAB\xCD'
+else:
+    # Scan command for normal continious read read
     scan_command = b'\x7E\x00\x08\x01\x00\x02\x01\xAB\xCD'
-    
-    ser.write(scan_command)
-    
-    time.sleep(0.1)
+    scan_duration = 10
 
-    while True:
-        if ser.read().decode() == '1' or ser.read().decode() == '':
-            break
+ser.write(scan_command)
 
-def recieve_data():
-    start_time = time.time()
-    data = ''
+time.sleep(0.1)
 
-    while (time.time() - start_time) <= scan_duration:
-        byte = ser.read().decode()
-        if byte == '\r':
-            break
-        else:
-            data += byte
+start_time = time.time()
+data = ''
 
-    return data
+while (time.time() - start_time) <= scan_duration:
+    byte = ser.read().hex()
+    data += byte
 
-while True:
-    start_scan()
-    print(recieve_data())
+# Process data a bit
+
+# Check for successful read and remove that code and the length info
+success = False
+if '0200' in data[:4]:
+    success = True
+    data = data[8:]
+
+# Remove data header for scanning
+if '003331' in data[:6]:
+    data = data[6:]
+
+if success and len(data) > 0:
+    print("Data:",data)
+else:
+    print("No data to display")
